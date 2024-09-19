a04378b9d59a5608dd85ce64b8a8e983fe125371
Kwesii2024 <181752632+Kwesii2024@users.noreply.github.com>
2024-09-19 12:46:00 +0100

    push

---
+++ Assets/Paralax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    GameObject player; //Reference to the player so we can track its position
+    Renderer rend; //Reference to the Renderer so we can modify its texture
+
+    float plsyerStartPos; 
+    public float speed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
